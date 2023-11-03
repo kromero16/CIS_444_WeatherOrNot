7bb1002a491290727019dbfbd4427b8d906349d2
kromero16 <kevkvn13@live.com>
2023-11-03 09:29:45 -0700

    made changes to forecast function

--- script.js
+++ script.js
@@ -1,62 +1,57 @@
-document.addEventListener("DOMContentLoaded", function() {
-  
-    const form = document.getElementById('city-form');
-    
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      
-      const city = document.getElementById('city').value;
-      
-      // Make the API request
-      fetchForecastForCity(city);
-    });
-  
-  });
-  
-  function fetchForecastForCity(city) {
-    const data = null;
+// Configuration object for API keys and defaults.
+const apiConfig = {
+  endpoint: 'https://weatherapi-com.p.rapidapi.com/forecast.json',
+  key: '5a1f892a6fmsh79b914fdd2d4469p1d85fejsnfd6c65fa4b63',
+  host: 'weatherapi-com.p.rapidapi.com',
+};
 
+// Function to fetch and display weather forecast for a city with a flexible number of days.
+function fetchForecastForCity(city, days = apiConfig.defaultDays) {
     const xhr = new XMLHttpRequest();
     xhr.withCredentials = true;
-    
+
     xhr.addEventListener('readystatechange', function () {
         if (this.readyState === this.DONE) {
-            const response = JSON.parse(this.responseText);
-            const forecastdays = response.forecast.forecastday;
-  
-            // Clear existing forecasts if any
-            const forecastContainer = document.getElementById('forecast-container');
-            forecastContainer.innerHTML = '';
-  
-            // Loop through each forecast day to capture the data
-            for (let i = 0; i < forecastdays.length; i++) {
-              const avgtemp_f = forecastdays[i].day.avgtemp_f;
-              const icon = forecastdays[i].day.condition.icon;
-  
-              // Create HTML elements dynamically
-              const forecastDiv = document.createElement('div');
-              forecastDiv.className = 'forecast';
-  
-              const tempElement = document.createElement('p');
-              tempElement.textContent = `Avg Temp: ${avgtemp_f}°F`;
-  
-              const iconElement = document.createElement('img');
-              iconElement.src = `https:${icon}`;
-  
-              // Append elements to the forecast div
-              forecastDiv.appendChild(tempElement);
-              forecastDiv.appendChild(iconElement);
-  
-              // Append forecast div to some parent container in your HTML
-              forecastContainer.appendChild(forecastDiv);
-            }
+            displayForecast(JSON.parse(this.responseText));
         }
     });
     
-    xhr.open('GET', `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${encodeURIComponent(city)}&days=3`);
-    xhr.setRequestHeader('X-RapidAPI-Key', '5a1f892a6fmsh79b914fdd2d4469p1d85fejsnfd6c65fa4b63');
-    xhr.setRequestHeader('X-RapidAPI-Host', 'weatherapi-com.p.rapidapi.com');
+    // Build the URL with query parameters.
+    const url = `${apiConfig.endpoint}?q=${encodeURIComponent(city)}&days=${days}`;
     
-    xhr.send(data);
-  }
-  
+    // Open a connection and set up the headers with API keys from the config.
+    xhr.open('GET', url);
+    xhr.setRequestHeader('X-RapidAPI-Key', apiConfig.key);
+    xhr.setRequestHeader('X-RapidAPI-Host', apiConfig.host);
+    
+    xhr.send(null);
+}
+
+// Helper function to create and display forecast elements on the page.
+function displayForecast(response) {
+  const forecastContainer = document.getElementById('forecast-container');
+  forecastContainer.innerHTML = ''; // Clear previous forecasts
+
+  // Iterate over the forecast days and create forecast elements.
+  response.forecast.forecastday.forEach(day => {
+      const forecastDiv = document.createElement('div');
+      forecastDiv.className = 'forecast';
+      forecastDiv.innerHTML = `
+          <p>Avg Temp: ${day.day.avgtemp_f}°F</p>
+          <img src="https:${day.day.condition.icon}">
+      `;
+      forecastContainer.appendChild(forecastDiv);
+  });
+}
+
+// Event listener to handle form submission.
+document.addEventListener("DOMContentLoaded", function() {
+  // When the form is submitted, prevent the default action and call fetchForecastForCity.
+  const form = document.getElementById('city-form');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const city = document.getElementById('city').value;
+    const days = document.getElementById('days').value || apiConfig.defaultDays;
+    fetchForecastForCity(city, days);
+  });
+});
